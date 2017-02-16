bef4a8e4ce3faa6e5f4d097e2a73bfaf59275a08
Sayem <afm.sayem@gmail.com>
2017-02-16 15:42:03 +0600

    feat(auth): add mail and facebook auth

---
+++ auth/facebook.js
@@ -0,0 +1,37 @@
+const request = require('request-promise');
+const User = require('../api/user/user.model');
+const config = require('../config/environment');
+const authUtils = require('./authutils');
+
+function createUser(res, profile) {
+  return User.query()
+    .insert({ name: profile.name, facebook: profile.id, email: profile.email })
+    .then(user => res.send({ token: authUtils.createJWT(user) }))
+    .catch(error => error);
+}
+
+exports.authenticate = function (req, res) {
+  const query = Object.assign({}, req.body, { fields: 'email,name' });
+  return request.get({
+    url: config.facebook.profile,
+    qs: query,
+    json: true,
+  }, (fberr, fbres, profile) => {
+    if (fberr) {
+      res.status(400).send({
+        message: fberr,
+      });
+    }
+
+    User.query()
+      .where('facebook', profile.id)
+      .then((user) => {
+        if (user.length) {
+          return res.status(200).send({ token: authUtils.createJWT(user[0]) });
+        }
+        return createUser(res, profile);
+      })
+      .catch(err => res.status(500).send(err));
+  });
+}
+
