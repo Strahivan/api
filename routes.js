4aeca2b1e8db896e220126a89eb1685a9bbfdf99
Sayem <afm.sayem@gmail.com>
2017-02-16 15:46:49 +0600

    feat(routes): add routes

---
+++ routes.js
@@ -0,0 +1,26 @@
+const country = require('./api/country');
+const request = require('./api/request');
+const trip = require('./api/trip');
+const user = require('./api/user');
+const zone = require('./api/zone');
+const auth = require('./auth');
+const signedUrl = require('./components/signing');
+
+module.exports = (app) => {
+  app.use('/countries', country);
+  app.use('/requests', request);
+  app.use('/trips', trip);
+  app.use('/zones', zone);
+  app.use('/users', user);
+
+  app.get('/upload', signedUrl);
+  app.use('/auth', auth);
+
+  app.use((err, req, res, next) => {
+    if (err) {
+      res.status(err.statusCode || err.status || 500).send(err.data || err.message || {});
+    } else {
+      next();
+    }
+  });
+};
