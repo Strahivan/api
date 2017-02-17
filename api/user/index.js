215486ee47c98ca0f31de1d3667d4e06bdd03b77
Sayem <afm.sayem@gmail.com>
2017-02-17 08:17:59 +0600

    feat(nested routes): add trips and requests nested routes for user

--- api/user/index.js
+++ api/user/index.js
@@ -3,11 +3,16 @@ const processQuery = require('../../components/middlewares/process-query');
 const authenticate = require('../../components/middlewares/authenticate');
 const User = require('./user.model');
 const UserController = require('./user.controller');
+const requestRouter = require('./user_request');
+const tripRouter = require('./user_trip');
 
 const router = new express.Router({ mergeParams: true });
-const controller = new UserController(User, 'user_id');
+
+router.use('/me/requests', requestRouter);
+router.use('/me/trips', tripRouter);
 
 router.use(authenticate);
+const controller = new UserController(User, 'user_id');
 
 router.get('/me', controller.show.bind(controller));
 router.get('/', processQuery, controller.index.bind(controller));
