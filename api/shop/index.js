318b685368df44af5bd6e0c1e1d3d8b6880715fb
Sayem <afm.sayem@gmail.com>
2017-03-23 18:54:34 +0600

    feat(shop): add support for shops

---
+++ api/shop/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const processQuery = require('../../components/middlewares/process-query');
+const authenticate = require('../../components/middlewares/authenticate');
+const addUser = require('../../components/middlewares/add-user');
+const Shop = require('./shop.model');
+const productRouter = require('../product');
+const BaseController = require('../base/base.controller');
+
+const controller = new BaseController(Shop, 'shop_id');
+
+const router = new express.Router({ mergeParams: true });
+
+router.use('/:shop_id/products', productRouter);
+
+router.get('/', processQuery, controller.index.bind(controller));
+router.get('/:shop_id', controller.show.bind(controller));
+
+router.use(authenticate);
+router.post('/', addUser('owner_id'), controller.create.bind(controller));
+router.put('/:shop_id', controller.update.bind(controller));
+router.patch('/:shop_id', controller.update.bind(controller));
+router.delete('/:shop_id', controller.destroy.bind(controller));
+
+module.exports = router;
+
---
+++ api/shop/shop.model.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const Base = require('../base/base.model');
+const schema = require('./shop.schema.json');
+
+class Shop extends Base {
+  static get tableName() {
+    return 'shop';
+  }
+
+  static get jsonSchema() {
+    return schema;
+  }
+
+  static get relationMappings() {
+    return {
+      owner: {
+        relation: Base.BelongsToOneRelation,
+        modelClass: path.normalize(`${__dirname}/../user/user.model`),
+        join: {
+          from: 'shop.owner_id',
+          to: 'user.id',
+        },
+      },
+    };
+  }
+}
+
+module.exports = Shop;
+
