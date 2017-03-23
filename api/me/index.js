c87a16bd41501d829347904c3576a04d0afcce7c
Sayem <afm.sayem@gmail.com>
2017-03-23 18:48:07 +0600

    refactor(single entry): reuse existing router for user specific data

--- api/me/index.js
+++ api/me/index.js
@@ -1,20 +1,24 @@
 const express = require('express');
-const processQuery = require('../../components/middlewares/process-query');
 const authenticate = require('../../components/middlewares/authenticate');
 const User = require('../user/user.model');
 const MeController = require('./me.controller');
-const requestRouter = require('./me_request.js');
-const tripRouter = require('./me_trip.js');
+const requestRouter = require('../request');
+const tripRouter = require('../trip');
+const shopRouter = require('../shop');
+const userFilter = require('../../components/middlewares/user-filter');
 
 const router = new express.Router({ mergeParams: true });
 const controller = new MeController(User);
 
 router.use(authenticate);
-router.use('/requests', requestRouter);
-router.use('/trips', tripRouter);
 
 router.get('/', controller.show.bind(controller));
 router.put('/', controller.update.bind(controller));
+
+router.use('/requests', userFilter('customer_id'), requestRouter);
+router.use('/shops', userFilter('owner_id'), shopRouter);
+router.use('/trips', userFilter('traveler_id'), tripRouter);
+
 // router.put('/unlink/:provider', controller.unlink.bind(controller));
 
 module.exports = router;
