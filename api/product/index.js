352c61d40237b43181995b8f762e9bd29adb4573
Sayem <afm.sayem@gmail.com>
2017-03-27 12:12:15 +0600

    refactor(middleware): ditch middlewares in favor of informed controller

--- api/product/index.js
+++ api/product/index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const processQuery = require('../../components/middlewares/process-query');
 const authenticate = require('../../components/middlewares/authenticate');
 const Product = require('./product.model');
-const addUser = require('../../components/middlewares/add-user');
 const requestRoutes = require('../request');
-const userFilter = require('../../components/middlewares/user-filter');
 const BaseController = require('../base/base.controller');
 
 const controller = new BaseController(Product, 'product_id');
@@ -17,8 +15,7 @@ router.get('/', processQuery, controller.index.bind(controller));
 router.get('/:product_id', controller.show.bind(controller));
 
 router.use(authenticate);
-router.use(userFilter('creator_id'));
-router.post('/', addUser('creator_id'), controller.create.bind(controller));
+router.post('/', controller.create.bind(controller));
 router.put('/:product_id', controller.update.bind(controller));
 router.patch('/:product_id', controller.update.bind(controller));
 router.delete('/:product_id', controller.destroy.bind(controller));
--- api/shop/index.js
+++ api/shop/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const processQuery = require('../../components/middlewares/process-query');
 const authenticate = require('../../components/middlewares/authenticate');
-const addUser = require('../../components/middlewares/add-user');
 const Shop = require('./shop.model');
 const productRouter = require('../product');
 const BaseController = require('../base/base.controller');
 
-const controller = new BaseController(Shop, 'shop_id');
+const controller = new BaseController(Shop, 'shop_id', 'owner_id');
 
 const router = new express.Router({ mergeParams: true });
 
@@ -16,7 +15,7 @@ router.get('/', processQuery, controller.index.bind(controller));
 router.get('/:shop_id', controller.show.bind(controller));
 
 router.use(authenticate);
-router.post('/', addUser('owner_id'), controller.create.bind(controller));
+router.post('/', controller.create.bind(controller));
 router.put('/:shop_id', controller.update.bind(controller));
 router.patch('/:shop_id', controller.update.bind(controller));
 router.delete('/:shop_id', controller.destroy.bind(controller));
