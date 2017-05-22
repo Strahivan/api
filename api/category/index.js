43cf075d01e35229d034aac720b09ca5069d9953
AFM Sayem <afm.sayem@gmail.com>
2017-05-22 12:33:49 +0800

    feat(category): expose endpoints to show products that belong to a category

--- api/category/index.js
+++ api/category/index.js
@@ -3,10 +3,13 @@ const Category = require('./category.model');
 const BaseController = require('../base/base.controller');
 const processQuery = require('../../components/middlewares/process-query');
 const authenticate = require('../../components/middlewares/authenticate');
+const productRouter = require('../product');
 
 const controller = new BaseController(Category, 'category_id');
 
 const router = new express.Router({ mergeParams: true });
+
+router.use('/:category_id/products', productRouter);
 
 router.get('/', processQuery, controller.index.bind(controller));
 router.get('/:category_id', controller.show.bind(controller));
