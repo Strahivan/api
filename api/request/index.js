3cb62ab09905d250feb2d6ac10183ccb5595d1c4
AFM Sayem <afm.sayem@gmail.com>
2017-05-26 16:45:03 +0800

    feat(middleware): add middleware to notify shop owners about new orders

--- api/request/index.js
+++ api/request/index.js
@@ -3,10 +3,8 @@ const processQuery = require('../../components/middlewares/process-query');
 const authenticate = require('../../components/middlewares/authenticate');
 const responseHandler = require('../../components/middlewares/respond');
 const Request = require('./request.model');
-const Shop = require('../shop/shop.model');
 const BaseController = require('../base/base.controller');
-const mailQueue = require('../../config/queue').mailQueue;
-const config = require('../../config/environment');
+const middlewares = require('./request.middlewares');
 
 const controller = new BaseController(Request, 'request_id', 'customer_id');
 
@@ -16,47 +14,9 @@ router.get('/', processQuery, controller.index.bind(controller), responseHandler
 router.get('/:request_id', controller.show.bind(controller), responseHandler);
 
 router.use(authenticate);
-router.post('/', controller.create.bind(controller), responseHandler);
-router.put('/:request_id', controller.update.bind(controller), notifyUser, responseHandler);
-router.patch('/:request_id', controller.update.bind(controller), notifyUser, responseHandler);
+router.post('/', controller.create.bind(controller), middlewares.notifyOrderCreated, responseHandler);
+router.put('/:request_id', controller.update.bind(controller), middlewares.notifyOrderChanged, responseHandler);
+router.patch('/:request_id', controller.update.bind(controller), middlewares.notifyOrderChanged, responseHandler);
 router.delete('/:request_id', controller.destroy.bind(controller), responseHandler);
-
-async function notifyUser(req, res, next) {
-  if (req.body.status) {
-    try {
-      const order = await Request.query()
-        .findById(req.params.request_id)
-        .eager('[customer, product]');
-
-      let email = order.customer.email;
-      let template = 'order-status';
-
-      if (req.body.status === 'completed') {
-        const shop = await Shop.query()
-          .findById(order.product.shop_id)
-          .eager('[owner]');
-
-        email = shop.owner.email;
-        template = 'order-status';
-      }
-      await mailQueue.add({
-        from: `Novelship <${config.mail.notify}>`,
-        to: email,
-        template: template,
-        context: {
-          order_url: `${process.env.WEBAPP_URL}/user/requests/${req.params.request_id}`,
-          order_number: order.id,
-          status: order.status,
-          product_name: order.product.name
-        }
-      });
-
-      next();
-    } catch (e) {
-      return next(e);
-    }
-  }
-  return next();
-}
 
 module.exports = router;
---
+++ api/request/request.middlewares.js
@@ -0,0 +1,77 @@
+const Shop = require('../shop/shop.model');
+const Product = require('../product/product.model');
+const mailQueue = require('../../config/queue').mailQueue;
+const config = require('../../config/environment');
+const Request = require('./request.model');
+
+async function notifyOrderCreated(req, res, next) {
+  try {
+    const recipients = [];
+    const product = await Product.query()
+      .findById(res.locals.data.product_id);
+
+    const shop = await Shop.query()
+      .findById(product.shop_id)
+      .eager('[owner]');
+
+    recipients.push(shop.owner.email);
+    recipients.push(config.mail.sales);
+
+    await mailQueue.add({
+      from: `Novelship <${config.mail.notify}>`,
+      to: recipients,
+      template: 'new-order',
+      context: {
+        store_url: `${config.webappUrl}/user/shops/${shop.id}`,
+        order_url: `${config.webappUrl}/user/shops/${shop.id}/requests/${res.locals.data.id}`,
+        product_url: `${config.webappUrl}/products/${res.locals.data.product_id}`,
+        product_name: product.name,
+        store_name: shop.name
+      }
+    });
+    next();
+  } catch (e) {
+    /* handle error */
+    return next(e);
+  }
+}
+
+async function notifyOrderChanged(req, res, next) {
+  if (req.body.status) {
+    try {
+      const order = await Request.query()
+        .findById(req.params.request_id)
+        .eager('[customer, product]');
+
+      let email = order.customer.email;
+      let template = 'order-status';
+
+      if (req.body.status === 'completed') {
+        const shop = await Shop.query()
+          .findById(order.product.shop_id)
+          .eager('[owner]');
+
+        email = shop.owner.email;
+        template = 'order-status';
+      }
+      await mailQueue.add({
+        from: `Novelship <${config.mail.notify}>`,
+        to: email,
+        template: template,
+        context: {
+          order_url: `${process.env.WEBAPP_URL}/user/requests/${req.params.request_id}`,
+          order_number: order.id,
+          status: order.status,
+          product_name: order.product.name
+        }
+      });
+
+      next();
+    } catch (e) {
+      return next(e);
+    }
+  }
+  return next();
+}
+
+module.exports = { notifyOrderChanged, notifyOrderCreated};
