1d820797ec055c1c70bfcc1087c7dd05b66c12d2
AFM Sayem <afm.sayem@gmail.com>
2017-06-21 19:28:44 +0800

    feat(price calculator): calculate price on server side middleware

--- api/product/product.middlewares.js
+++ api/product/product.middlewares.js
@@ -1,10 +1,32 @@
 const Country = require('../country/country.model');
 
+function calculateMargin(cost, tiers) {
+  const marginSpread = tiers.map(tier => {
+    const rate = tier.rate / 100;
+    if (cost > tier.min && cost < tier.max) {
+      // if it's greater than min but less than max
+      return Math.abs(cost - tier.max) * rate;
+    } else if (cost <= tier.min) {
+      // if the product is lower than the price bracket
+      return 0;
+    } else if (tier.max === undefined) {
+      // if highest tier has been reached
+      return (cost - tier.min) * rate;
+    }
+    // by default, return the rate for the specific tier
+    return (tier.max - tier.min) * rate;
+  });
+
+  return marginSpread.reduce((sum, current) => {
+    return sum + current;
+  }, 0);
+}
+
 async function calculatePrice(req, res, next) {
   try {
     const country = await Country.query()
       .findById(req.body.source_id);
-    req.body.price = req.body.cost + (req.body.cost * 0.15) + (req.body.cost * 0.07) + (req.body.weight * country.ems_fee) + (req.body.local_delivery_fee || 0) + (req.body.price_override || 0);
+    req.body.price = req.body.cost + calculateMargin(req.body.cost, country.tiers) + (req.body.cost * 0.07) + (req.body.weight * country.ems_fee) + (req.body.local_delivery_fee || 0) + (req.body.price_override || 0);
     next();
   } catch (e) {
     return next(e);
