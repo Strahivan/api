563423bf0aaec759625bd0a63418c4eb1efdfefe
AFM Sayem <afm.sayem@gmail.com>
2017-06-21 21:13:54 +0800

    feat(ceiling): ceiling all the prices to immediate tenths

--- api/product/product.middlewares.js
+++ api/product/product.middlewares.js
@@ -1,5 +1,28 @@
 const Country = require('../country/country.model');
 
+function decimalAdjust(type, value, exp) {
+  // If the exp is undefined or zero...
+  if (typeof exp === 'undefined' || +exp === 0) {
+    return Math[type](value);
+  }
+  value = +value;
+  exp = +exp;
+  // If the value is not a number or the exp is not an integer...
+  if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
+    return NaN;
+  }
+  // Shift
+  value = value.toString().split('e');
+  value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
+  // Shift back
+  value = value.toString().split('e');
+  return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
+}
+
+function getCeiling(value, exp) {
+  return decimalAdjust('ceil', value, exp);
+}
+
 function calculateMargin(cost, tiers) {
   const marginSpread = tiers.map(tier => {
     const rate = tier.rate / 100;
@@ -26,7 +49,7 @@ async function calculatePrice(req, res, next) {
   try {
     const country = await Country.query()
       .findById(req.body.source_id);
-    req.body.price = req.body.cost + calculateMargin(req.body.cost, country.tiers) + (req.body.cost * 0.07) + (req.body.weight * country.ems_fee) + (req.body.local_delivery_fee || 0) + (req.body.price_override || 0);
+    req.body.price = getCeiling(req.body.cost + calculateMargin(req.body.cost, country.tiers) + (req.body.cost * 0.07) + (req.body.weight * country.ems_fee) + (req.body.local_delivery_fee || 0) + (req.body.price_override || 0), -1);
     next();
   } catch (e) {
     return next(e);
