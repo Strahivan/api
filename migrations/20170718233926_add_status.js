6a05253ade64b3f31aa8a0fb0379f40ac6065696
AFM Sayem <afm.sayem@gmail.com>
2017-07-21 16:52:57 +0800

    feat(rejection): add rejection status for products

--- migrations/20170718233926_add_status.js
+++ migrations/20170718233926_add_status.js
@@ -1,7 +1,7 @@
 function up(knex, Promise) {
   return knex.schema
     .raw('ALTER TABLE request DROP CONSTRAINT request_status_check')
-    .raw('ALTER TABLE request ADD CONSTRAINT request_status_check CHECK (status = ANY (ARRAY[\'pending\'::text, \'confirmed\'::text, \'processing\'::text, \'delivering\'::text, \'ready\'::text, \'completed\'::text, \'failed\'::text, \'verify\'::text, \'approval\'::text]))');
+    .raw('ALTER TABLE request ADD CONSTRAINT request_status_check CHECK (status = ANY (ARRAY[\'pending\'::text, \'confirmed\'::text, \'processing\'::text, \'delivering\'::text, \'ready\'::text, \'completed\'::text, \'failed\'::text, \'verify\'::text, \'approval\'::text, \'rejected\'::text]))');
 }
 
 function down(knex, Promise) {
