684af7cf99bd498774d64a9eeb6b748310a1a33d
AFM Sayem <afm.sayem@gmail.com>
2017-08-14 14:18:33 +0800

    feat(status): map messages with each status

---
+++ components/status-map.js
@@ -0,0 +1,35 @@
+module.exports = {
+  canceled: {
+    message: 'Your order was canceled.'
+  },
+  completed: {
+    message: 'Your order was marked as complete.'
+  },
+  confirmed: {
+    message: 'Thank you! Your order has been confirmed.'
+  },
+  delivering: {
+    message: 'Huzzah! We are now delivering your item.'
+  },
+  failed: {
+    message: 'Sorry. We were unable to fulfill your order.'
+  },
+  pending: {
+    message: 'We got your request. Hang in there while we verify the availability of the item. You\'ll get a confirmation in half an hour.'
+  },
+  processing: {
+    message: 'We have began processing your order.'
+  },
+  ready: {
+    message: 'Ahoy! Your item is now ready for pickup!'
+  },
+  verify: {
+    message: 'Our in house goblins are verifying the amount transferred. We\'ll confirm the order soon. Hold tight!'
+  },
+  approval: {
+    message: 'Hello! We just verified that we are able to get the item. We also quoted you the price. Once you have made payment, we will move forward with the order.'
+  },
+  rejected: {
+    message: 'We are sorry for not being able to meet your needs. We promise we\'ll do better next time.'
+  }
+};
