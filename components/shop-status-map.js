f50fc4100b49e1e4d99577099d3bc9ec89c219c4
AFM Sayem <afm.sayem@gmail.com>
2017-08-14 14:30:40 +0800

    feat(email): map shop owner messages

---
+++ components/shop-status-map.js
@@ -0,0 +1,9 @@
+module.exports = {
+  pending: {
+    message: 'A new custom order is waiting for your verification. Please check availability of the item and set the status to approval.'
+  },
+  verify: {
+    message: 'Congratulations! A user just purchased an item through bank transfer. Please verify that you received the amount and set the order to confirmed.'
+  }
+};
+
