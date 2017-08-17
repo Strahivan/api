400b051ceb4b247bf79c85b76657e8840d582b82
AFM Sayem <afm.sayem@gmail.com>
2017-08-17 11:58:14 +0800

    feat(shipping): add shipping status to requests

---
+++ migrations/20170817114925_shipping_flag.js
@@ -0,0 +1,13 @@
+function up(knex, Promise) {
+  return knex.schema
+    .raw('ALTER TABLE request DROP CONSTRAINT request_status_check')
+    .raw('ALTER TABLE request ADD CONSTRAINT request_status_check CHECK (status = ANY (ARRAY[\'pending\'::text, \'confirmed\'::text, \'processing\'::text, \'delivering\'::text, \'ready\'::text, \'completed\'::text, \'failed\'::text, \'verify\'::text, \'approval\'::text, \'rejected\'::text, \'canceled\'::text, \'shipping\'::text]))');
+}
+
+function down(knex, Promise) {
+  return knex.schema
+    .raw('ALTER TABLE request DROP CONSTRAINT request_status_check')
+    .raw('ALTER TABLE request ADD CONSTRAINT request_status_check CHECK (status = ANY (ARRAY[\'pending\'::text, \'confirmed\'::text, \'processing\'::text, \'delivering\'::text, \'ready\'::text, \'completed\'::text, \'failed\'::text, \'verify\'::text, \'approval\'::text, \'rejected\'::text, \'canceled\'::text]))');
+}
+
+module.exports = { up, down };
