2e55aa7f5d4070ef98f4464256f7c6a17b025a59
AFM Sayem <afm.sayem@gmail.com>
2017-08-22 19:15:26 +0800

    feat(full text search): modify search query filter

--- components/filters/text-search.js
+++ components/filters/text-search.js
@@ -4,6 +4,6 @@ module.exports = function(propertyRef, value, modelClass) {
 
   return {
     method: 'whereRaw',
-    args: [`similarity(${columnName}, ?) > 0.2`, value.toLowerCase()]
+    args: [`${columnName} @@ to_tsquery(?)`, value.toLowerCase()]
   };
 };
