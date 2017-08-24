1fb413677923efe153f5d16fb18d5f4248259e08
AFM Sayem <afm.sayem@gmail.com>
2017-08-24 16:24:31 +0800

    fix(text search): split search text by whitespace

--- components/filters/text-search.js
+++ components/filters/text-search.js
@@ -2,8 +2,10 @@ module.exports = function(propertyRef, value, modelClass) {
   const formatter = modelClass.formatter();
   const columnName = formatter.wrap(propertyRef.fullColumnName());
 
+  const searchValue = value.split(' ').map(val => val.toLowerCase()).join(' | ');
+
   return {
     method: 'whereRaw',
-    args: [`${columnName} @@ to_tsquery(?)`, value.toLowerCase()]
+    args: [`${columnName} @@ to_tsquery(?)`, searchValue]
   };
 };
