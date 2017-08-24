f4a4e75bad56ce67a3974987d2476a1a78c8800f
AFM Sayem <afm.sayem@gmail.com>
2017-08-24 16:30:25 +0800

    fix(text search): perform and query instead of or query

--- components/filters/text-search.js
+++ components/filters/text-search.js
@@ -2,7 +2,7 @@ module.exports = function(propertyRef, value, modelClass) {
   const formatter = modelClass.formatter();
   const columnName = formatter.wrap(propertyRef.fullColumnName());
 
-  const searchValue = value.split(' ').map(val => val.toLowerCase()).join(' | ');
+  const searchValue = value.split(' ').map(val => val.toLowerCase()).join(' & ');
 
   return {
     method: 'whereRaw',
