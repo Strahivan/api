e788388fac46f20fdb526cfd6786471fe23e4570
AFM Sayem <afm.sayem@gmail.com>
2017-11-23 13:02:13 +0800

    feat(shop status): update shop emails for second payment

--- components/shop-status-map.js
+++ components/shop-status-map.js
@@ -4,6 +4,15 @@ module.exports = {
   },
   verify: {
     message: 'Congratulations! A user just purchased an item through bank transfer. Please verify that you received the amount and set the order to confirmed.'
+  },
+  confirmed: {
+    message: 'Congratulations! A user just purchased an item through paypal. Please set the order to processing when you have the item in your hands.'
+  },
+  ready_for_delivery: {
+    message: 'A user just paid the second installment of her order. Please arrange for delivery for the item.'
+  },
+  verify_pending_payment: {
+    message: 'A user just paid the second installment of her order via bank transfer. Please verify receipt and then arrange for delivery for the item.'
   }
 };
 
