d01a6a54c4f3a2e58546d4d5ec310b7e22b6069d
AFM Sayem <afm.sayem@gmail.com>
2017-12-04 14:53:18 +0800

    feat(referral code): add referral code generator

--- auth/authutils.js
+++ auth/authutils.js
@@ -1,8 +1,10 @@
 const moment = require('moment');
 const jwt = require('jwt-simple');
 const config = require('../config/environment');
+const User = require('../api/user/user.model');
+const randomstring = require('randomstring');
 
-exports.createJWT = function(user) {
+function createJWT(user) {
   const payload = {
     sub: user.id,
     iat: moment().unix(),
@@ -10,5 +12,21 @@ exports.createJWT = function(user) {
   };
 
   return jwt.encode(payload, config.token);
-};
+}
 
+async function getReferralCode() {
+  const referralCode = randomstring.generate({
+    length: 6,
+    charset: 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefgijkmnpqrstuvwxyz23456789'
+  });
+
+  const user = await User.query()
+    .where({ referral_code: referralCode});
+
+  if (user.length === 0) {
+    return referralCode;
+  }
+  return getReferralCode();
+}
+
+module.exports = {createJWT, getReferralCode};
